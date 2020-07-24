6fa9bb057ec82017dd3a5d3a22c1a851bf4334a3
Przemyslaw Szymanski <przemyslaw-szymanski@intel.com>
2020-07-24 15:37:38 +0200

    zlib

---
+++ src/ThirdParty/zlib/_build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/Workspace/projects/vke/src/ThirdParty/zlib
+# Build directory: D:/Workspace/projects/vke/src/ThirdParty/zlib/_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "D:/Workspace/projects/vke/src/ThirdParty/zlib/CMakeLists.txt;235;add_test;D:/Workspace/projects/vke/src/ThirdParty/zlib/CMakeLists.txt;0;")
