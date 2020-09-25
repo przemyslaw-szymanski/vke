3211cd01d91fd46e5a0f71714ddf6347e18d2b1a
Przemyslaw Szymanski <przemyslaw-szymanski@intel.com>
2020-09-25 20:33:15 +0200

    update

---
+++ src/RenderSystem/D3D12/CShaderCompiler.cpp
@@ -0,0 +1,335 @@
+#include "RenderSystem/Vulkan/CShaderCompiler.h"
+#if VKE_USE_DIRECTX_SHADER_COMPILER
+#include "ThirdParty/dxc/include/dxc/dxcapi.h"
+#include "ThirdParty/dxc/include/dxc/Support/dxcapi.use.h"
+
+#include <codecvt>
+#include <locale>
+
+namespace VKE
+{
+#if VKE_WINDOWS
+#   define VKE_HR_OK S_OK
+#   define VKE_HR_FAIL S_FALSE
+#else
+#   define VKE_HR_OK 0
+#   define VKE_HR_FAIL = -1
+#endif
+
+    namespace RenderSystem
+    {
+        struct SDXC
+        {
+            dxc::DxcDllSupport          Support;
+            IDxcCompiler*               pCompiler = nullptr;
+            IDxcLibrary*                pLibrary = nullptr;
+            IDxcContainerReflection*    pContainerReflection = nullptr;
+            IDxcVersionInfo*            pVersionInfo = nullptr;
+            IDxcVersionInfo2*           pVersionInfo2 = nullptr;
+        };
+
+        static SDXC g_DXC;
+
+        CShaderCompiler::CShaderCompiler(CShaderManager* pMgr) :
+            m_pShaderMgr{ pMgr }
+        {
+
+        }
+
+        CShaderCompiler::~CShaderCompiler()
+        {
+            Destroy();
+        }
+
+        void CShaderCompiler::Destroy()
+        {
+            if( m_isCreated )
+            {
+                g_DXC.pLibrary->Release();
+                g_DXC.pCompiler->Release();
+                g_DXC.Support.Cleanup();
+                m_isCreated = false;
+            }
+        }
+
+        Result CShaderCompiler::Create(const SShaderCompilerDesc& Desc)
+        {
+            Result ret = VKE_FAIL;
+            m_Desc = Desc;
+            HRESULT res = g_DXC.Support.Initialize();
+            if( res == VKE_HR_OK )
+            {
+                res = g_DXC.Support.CreateInstance(CLSID_DxcCompiler, &g_DXC.pCompiler);
+                if (res == VKE_HR_OK)
+                {
+                    res = g_DXC.Support.CreateInstance(CLSID_DxcCompiler, &g_DXC.pVersionInfo);
+                    if (res == VKE_HR_OK)
+                    {
+                        res = g_DXC.Support.CreateInstance(CLSID_DxcCompiler, &g_DXC.pVersionInfo2);
+                        if (res == VKE_HR_OK)
+                        {
+                            res = g_DXC.Support.CreateInstance(CLSID_DxcLibrary, &g_DXC.pLibrary);
+                            if (res == VKE_HR_OK)
+                            {
+                                res = g_DXC.Support.CreateInstance(CLSID_DxcContainerReflection, &g_DXC.pContainerReflection);
+                                if (res == VKE_HR_OK)
+                                {
+                                    ret = VKE_OK;
+                                    m_isCreated = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                /*(
+                    DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&compiler)));
+                (
+                    DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&compiler_version_info)));
+                (
+                    DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&compiler_version_info2)));
+                (DxcCreateInstance(CLSID_DxcLibrary, IID_PPV_ARGS(&library)));*/
+            }
+            return ret;
+        }
+
+        size_t vke_force_inline ConvertCSTRToWCSTR(wchar_t* pDst, uint32_t dstSize, cstr_t pSrc, uint32_t srcSize)
+        {
+            size_t ret;
+            mbstowcs_s(&ret, pDst, dstSize, pSrc, srcSize);
+            return ret;
+        }
+
+        void GetProfile(SHADER_TYPE type, SHADER_PROFILE profile, wchar_t* pOut)
+        {
+            static cwstr_t aTypes[ShaderTypes::_MAX_COUNT] =
+            {
+                L"vs",
+                L"hs",
+                L"ds",
+                L"gs",
+                L"ps",
+                L"cs",
+            };
+            static cwstr_t aProfiles[ShaderProfiles::_MAX_COUNT] =
+            {
+                L"6_0",
+                L"6_1",
+                L"6_2",
+                L"6_3",
+                L"6_4",
+            };
+
+            //wchar_t* pOut = *ppOut;
+            pOut[0] = aTypes[type][0];
+            pOut[1] = aTypes[type][1];
+            pOut[2] = L'_';
+            pOut[3] = aProfiles[profile][0];
+            pOut[4] = aProfiles[profile][1];
+            pOut[5] = aProfiles[profile][2];
+            pOut[6] = 0;
+        }
+
+        std::wstring GetProfile2(SHADER_TYPE type, SHADER_PROFILE profile)
+        {
+            static cwstr_t aTypes[ShaderTypes::_MAX_COUNT] =
+            {
+                L"vs",
+                L"hs",
+                L"ds",
+                L"gs",
+                L"ps",
+                L"cs",
+            };
+            static cwstr_t aProfiles[ShaderProfiles::_MAX_COUNT] =
+            {
+                L"6_0",
+                L"6_1",
+                L"6_2",
+                L"6_3",
+                L"6_4",
+            };
+
+            std::wstring ret;
+            ret = aTypes[type] + std::wstring(L"_") + aProfiles[profile];
+            return ret;
+        }
+
+        SShaderDesc::NameWString GetProfile(SHADER_TYPE type, SHADER_PROFILE profile)
+        {
+            static cwstr_t aTypes[ShaderTypes::_MAX_COUNT] =
+            {
+                L"vs",
+                L"hs",
+                L"ds",
+                L"gs",
+                L"ps",
+                L"cs",
+            };
+            static cwstr_t aProfiles[ShaderProfiles::_MAX_COUNT] =
+            {
+                L"6_0",
+                L"6_1",
+                L"6_2",
+                L"6_3",
+                L"6_4",
+            };
+
+            SShaderDesc::NameWString ret;
+            ret = aTypes[type];
+            ret += SShaderDesc::NameWString(L"_");
+            ret += aProfiles[profile];
+            return ret;
+        }
+
+        void CopyBytecode(const void* pData, uint32_t dataSize, SCompileShaderData* pOut)
+        {
+            auto& vData = pOut->vShaderBinary;
+            pOut->codeByteSize = dataSize;
+
+            vData.reserve(dataSize);
+            const uint8_t* pSrc = (const uint8_t*)pData;
+            const uint8_t* pEnd = pSrc + dataSize;
+
+            while (pSrc != pEnd)
+            {
+                vData.push_back(*pSrc++);
+            }
+        }
+
+        std::string ToUtf8(const wchar_t* pStr)
+        {
+            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> conv;
+            return conv.to_bytes(pStr);
+        }
+
+        std::wstring ToUtf16(const char* pStr)
+        {
+            std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> conv;
+            return conv.from_bytes(pStr);
+        }
+
+        size_t ToUtf16(cstr_t pSrc, const uint32_t srcSize, wchar_t* pDst, const uint32_t dstLength)
+        {
+            size_t ret;
+            mbstowcs_s(&ret, pDst, dstLength, pSrc, srcSize);
+            return ret;
+        }
+
+        Result CShaderCompiler::Compile(const SCompileShaderInfo& Info, SCompileShaderData* pOut)
+        {
+            Result ret = VKE_FAIL;
+            IDxcBlobEncoding* pTextBlob;
+            IDxcOperationResult* pCompilerResult;
+
+            const auto pText = Info.pBuffer;
+            const auto textSize = Info.bufferSize;
+            HRESULT res = g_DXC.pLibrary->CreateBlobWithEncodingFromPinned(pText, textSize, CP_UTF8, &pTextBlob);
+            if (res == VKE_HR_OK)
+            {
+                vke_vector<const wchar_t*> vArgs;
+                vArgs.reserve(32);
+
+                vArgs.push_back(L"-Ges");
+                vArgs.push_back(L"-WX");
+                vArgs.push_back(L"-H");
+                vArgs.push_back(L"-Vi");
+#if VKE_RENDERER_DEBUG
+                vArgs.push_back(L"-Zi");
+                vArgs.push_back(L"-Od");
+#else
+                vArgs.push_back(L"-O3");
+#endif
+#if VKE_VULKAN_RENDERER
+                vArgs.push_back( L"-spirv" );
+                vArgs.push_back(L"-fvk-use-gl-layout");
+                vArgs.push_back(L"-fspv-target-env=vulkan1.1");
+                //vArgs.push_back(L"-fvk-bind-globals 128 0");
+#endif
+
+                const SShaderDesc::NameWString Name = Info.pDesc->Name;
+                const SShaderDesc::NameWString EntryPoint = (Info.pDesc->EntryPoint);
+                const SShaderDesc::NameWString Profile = GetProfile(Info.pDesc->type, Info.pDesc->profile);
+
+                const auto pName = Name;//Name.c_str();
+                const auto pEntryPoint = EntryPoint;
+                const auto pProfile = Profile;
+
+                Utils::TCDynamicArray< DxcDefine > vDefines;
+                for (uint32_t i = 0; i < Info.pDesc->vDefines.GetCount(); ++i)
+                {
+                    DxcDefine Define;
+                    Define.Name = Info.pDesc->vDefines[i].Name;
+                    Define.Value = Info.pDesc->vDefines[i].Value;
+                    vDefines.PushBack(Define);
+                }
+
+                res = g_DXC.pCompiler->Compile(pTextBlob, pName, pEntryPoint, pProfile,
+                    &vArgs[0], (uint32_t)vArgs.size(), vDefines.GetData(), vDefines.GetCount(),
+                    nullptr, &pCompilerResult);
+                if (res == VKE_HR_OK)
+                {
+                    pCompilerResult->GetStatus(&res);
+                    IDxcBlobEncoding* pErrorBlob;
+                    pCompilerResult->GetErrorBuffer(&pErrorBlob);
+                    cstr_t pErr = (cstr_t)pErrorBlob->GetBufferPointer();
+                    // Treat all warnings as errors
+                    if (pErr)
+                    {
+                        VKE_LOG_ERR("\n--------------------------------------------------------\n" <<
+                            "Shader: \"" << Info.pDesc->Name.GetData() << "\" compilation errors:\n\n" << pErr <<
+                            "\n--------------------------------------------------------\n");
+                        res = VKE_HR_FAIL;
+                    }
+
+                    if (res == VKE_HR_OK)
+                    {
+                        IDxcBlob* pBytecodeBlob;
+                        pCompilerResult->GetResult(&pBytecodeBlob);
+                        {
+                            CopyBytecode(pBytecodeBlob->GetBufferPointer(), (uint32_t)pBytecodeBlob->GetBufferSize(), pOut);
+                        }
+                        pBytecodeBlob->Release();
+                        ret = VKE_OK;
+                    }
+
+                    pErrorBlob->Release();
+                    pCompilerResult->Release();
+                    pTextBlob->Release();
+                }
+            }
+            VKE_ASSERT(ret == VKE_OK, "");
+            return ret;
+        }
+
+        Result CShaderCompiler::ConvertToBinary(const SLinkShaderData& LinkData, SShaderBinaryData* pOut)
+        {
+            Result ret = VKE_FAIL;
+            VKE_ASSERT(ret == VKE_OK, "");
+            return ret;
+        }
+
+        Result CShaderCompiler::WriteToHeaderFile(const char* pFileName, const SCompileShaderInfo& Info,
+                                                  const SLinkShaderData& Data)
+        {
+            Result ret = VKE_FAIL;
+            VKE_ASSERT(ret == VKE_OK, "");
+            return ret;
+        }
+
+        Result CShaderCompiler::WriteToBinaryFile(const char* pFileName, const SCompileShaderInfo& Info, const SLinkShaderData& Data)
+        {
+            Result ret = VKE_FAIL;
+            VKE_ASSERT(ret == VKE_OK, "");
+            return ret;
+        }
+
+        Result CShaderCompiler::WriteToBinaryFile(cstr_t pFileName, const std::vector<uint32_t>& vBinary )
+        {
+            //cstr_t const pExt = Platform::File::GetExtension( pFileName );
+
+            return VKE_OK;
+        }
+
+    } // RenderSystem
+} // VKE
+#endif // VKE_USE_GLSL_COMPILER
---
+++ src/RenderSystem/Resources/CShader.cpp
@@ -0,0 +1,162 @@
+#include "RenderSystem/Vulkan/Resources/CShader.h"
+#if VKE_VULKAN_RENDERER
+#include "RenderSystem/Managers/CShaderManager.h"
+
+namespace VKE
+{
+    namespace RenderSystem
+    {
+        /*static const EShLanguage g_aLanguages[ EShLangCount ] =
+        {
+            EShLangVertex,
+            EShLangTessControl,
+            EShLangTessEvaluation,
+            EShLangGeometry,
+            EShLangFragment,
+            EShLangCompute
+        };*/
+
+        CShader::CShader(CShaderManager* pMgr, SHADER_TYPE) :
+            m_pMgr{ pMgr }
+        {
+        }
+
+        CShader::~CShader()
+        {
+        }
+
+
+        hash_t CShader::CalcHash(const SShaderDesc& Desc)
+        {
+            const hash_t h1 = Core::CResource::CalcHash( Desc.FileInfo );
+            const hash_t h2 = Core::CResource::CalcHash( Desc.EntryPoint.GetData() );
+            const hash_t h3 = Desc.type;
+            const hash_t h4 = h1 ^ ( h2 << 1 );
+            const hash_t h5 = h4 ^ ( h3 << 1 );
+            const hash_t h6 = h5 ^ ( h4 << 1 );
+            hash_t incHash = 0;
+            hash_t prepHash = 0;
+            for (uint32_t i = 0; i < Desc.vIncludes.GetCount(); ++i)
+            {
+                const hash_t h = Core::CResource::CalcHash( Desc.vIncludes[ i ] );
+                incHash = h ^ ( incHash << 1 );
+            }
+
+            for (uint32_t i = 0; i < Desc.vPreprocessor.GetCount(); ++i)
+            {
+                const hash_t h = Core::CResource::CalcHash( Desc.vPreprocessor[ i ] );
+                prepHash = h ^ ( prepHash << 1 );
+            }
+            const hash_t hash = h6 ^ ( incHash ) ^ ( prepHash );
+            SHandle Handle;
+            Handle.value = 0;
+            Handle.hash = hash;
+            Handle.type = Desc.type;
+            return (hash_t)Handle.value;
+        }
+
+        void CShader::Init(const SShaderDesc& Info, const hash_t& hash)
+        {
+            {
+                //m_CompilerData.pShader = ::new( &m_CompilerData.ShaderMemory ) glslang::TShader( g_aLanguages[ Info.type ] );
+                //m_CompilerData.pProgram = ::new( &m_CompilerData.ProgramMemory ) glslang::TProgram();
+                m_Desc = Info;
+                if( Info.pData )
+                {
+                    m_Data = *Info.pData;
+                }
+                this->m_hObject.handle = hash;
+                _SetResourceState( Core::ResourceStates::INITIALIZED );
+            }
+        }
+
+        void CShader::Release()
+        {
+            //m_CompilerData.Release();
+            _SetResourceState( Core::ResourceStates::INVALID );
+
+            if( this->GetRefCount() == 0 )
+            {
+                m_Data.pCode = nullptr;
+                m_Data.codeSize = 0;
+                m_Data.stage = ShaderCompilationStages::UNKNOWN;
+                m_pFile = nullptr;
+                m_pMgr->_FreeShader( this );
+            }
+        }
+
+        Result CShader::Compile()
+        {
+            VKE_ASSERT( m_pMgr, "Shader manager is not set." );
+            Result res = VKE_OK;
+            if( this->m_hDDIObject == DDI_NULL_HANDLE )
+            {
+                CShader* pThis = this;
+                res = m_pMgr->_PrepareShaderTask( &pThis );
+            }
+            return res;
+        }
+
+        void CShader::_SetFile( Core::FilePtr pFile )
+        {
+            VKE_ASSERT( m_pFile.IsNull(), "File already set. Be sure a shader is properly created." );
+            m_pFile = pFile;
+            m_Data.pCode = pFile->GetData();
+            m_Data.codeSize = pFile->GetDataSize();
+            m_Data.stage = ShaderCompilationStages::HIGH_LEVEL_TEXT;
+            _AddResourceState( Core::ResourceStates::LOADED );
+        }
+
+
+        //CShaderProgram::CShaderProgram(CShaderManager* pMgr) :
+        //    m_pMgr( pMgr )
+        //{
+        //}
+
+        //CShaderProgram::~CShaderProgram()
+        //{}
+
+        //void CShaderProgram::operator delete(void* pProgram)
+        //{
+        //    CShaderProgram* pThis = static_cast< CShaderProgram* >( pProgram );
+        //    VKE_ASSERT( pThis, "Can not delete null pointer." );
+        //    pThis->Release();
+        //}
+
+        //void CShaderProgram::Release()
+        //{
+        //    for( uint32_t i = 0; i < ShaderTypes::_MAX_COUNT; ++i )
+        //    {
+        //        m_Desc.apShaders[ i ] = nullptr;
+        //    }
+        //    _SetResourceState( Core::ResourceStates::INVALID );
+        //    if( this->GetRefCount() == 0 )
+        //    {
+        //        //m_pMgr->_FreeProgram( this );
+        //    }
+        //}
+
+        //void CShaderProgram::Init(const SShaderProgramDesc& Desc)
+        //{
+        //    m_Desc = Desc;
+        //    _SetResourceState( Core::ResourceStates::CREATED );
+        //    SHash Hash;
+        //    Hash += Core::CResource::CalcHash( Desc.FileInfo );
+
+        //    for( uint32_t i = 0; i < ShaderTypes::_MAX_COUNT; ++i )
+        //    {
+        //        //h2 ^= ( CalcHash( Desc.apEntryPoints[ i ] ) << 1 );
+        //        Hash += Desc.apEntryPoints[ i ];
+        //        if( Desc.apShaders[ i ].IsValid() )
+        //        {
+        //            //h3 ^= ( Desc.apShaders[ i ]->GetHandle() << 1 );
+        //            Hash += Desc.apShaders[ i ]->GetHandle().handle;
+        //        }
+        //    }
+
+        //    this->m_hObject = Hash.value;
+        //}
+
+    } // RenderSystem
+} // VKE
+#endif // VKE_VULKAN_RENDERER
